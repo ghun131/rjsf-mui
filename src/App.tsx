96478e5c17670bf9b0befeb0bcc1eb1b4bcd8ef1
sylvianguyen02 <uyennguyenngocbich0202@gmail.com>
2022-05-25 14:36:10 +0700

    testing customized widgets

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import "froala-editor/css/froala_style.min.css";
 import "froala-editor/css/froala_editor.pkgd.min.css";
 import FroalaEditorComponent from "react-froala-wysiwyg";
 
+
+
+
 const App = (): JSX.Element => {
   return (
     <div className='App'>
